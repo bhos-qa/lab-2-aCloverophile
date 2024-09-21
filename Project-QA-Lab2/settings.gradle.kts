520ed951e0c79ecc1d48fe156e6f1fb458089d55
aCloverophile <122263705+aCloverophile@users.noreply.github.com>
2024-09-21 18:40:13 +0400

    Lab2 works done

---
+++ Project-QA-Lab2/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Project-QA-Lab2"
+
---
+++ Project-QA-Lab2/src/main/java/CloverTestClass.java
@@ -0,0 +1,12 @@
+public class CloverTestClass {
+
+    public static int calculateSum(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int result = calculateSum(44, 55);
+        System.out.println("The sum is: " + result);
+    }
+
+}
